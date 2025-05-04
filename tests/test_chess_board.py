82a578f5c26fe6d1d2442d63f8b08d6892a6defe
Payam Yektamaram <payamyek@gmail.com>
2025-05-04 18:54:31 -0400

    fix: tests

--- tests/test_chess_board.py
+++ tests/test_chess_board.py
@@ -6,22 +6,22 @@ def test_bitboard_get_position_mask():
     chess_board = ChessBoard()
 
     assert (
-        chess_board.get_position_mask(Rank.A, File.EIGHT)
+        chess_board.get_position_mask(Rank.EIGHT, File.A)
         == 0b_10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
     )
 
     assert (
-        chess_board.get_position_mask(Rank.H, File.EIGHT)
+        chess_board.get_position_mask(Rank.EIGHT, File.H)
         == 0b_00000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000
     )
 
     assert (
-        chess_board.get_position_mask(Rank.A, File.ONE)
+        chess_board.get_position_mask(Rank.ONE, File.A)
         == 0b_00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000000
     )
 
     assert (
-        chess_board.get_position_mask(Rank.H, File.ONE)
+        chess_board.get_position_mask(Rank.ONE, File.H)
         == 0b_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000001
     )
 
@@ -29,60 +29,60 @@ def test_bitboard_get_position_mask():
 def test_bitboard_get_piece_at_position():
     chess_board = ChessBoard()
 
-    assert chess_board.get_piece_at_position(Rank.A, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.A) == ChessPiece(
         "rook", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.B, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.B) == ChessPiece(
         "knight", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.C, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.C) == ChessPiece(
         "bishop", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.D, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.D) == ChessPiece(
         "queen", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.E, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.E) == ChessPiece(
         "king", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.F, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.F) == ChessPiece(
         "bishop", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.G, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.G) == ChessPiece(
         "knight", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.H, File.EIGHT) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.EIGHT, File.H) == ChessPiece(
         "rook", colour="black"
     )
-    assert chess_board.get_piece_at_position(Rank.A, File.SEVEN) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.SEVEN, File.A) == ChessPiece(
         "pawn", colour="black"
     )
 
-    assert chess_board.get_piece_at_position(Rank.A, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.A) == ChessPiece(
         "rook", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.B, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.B) == ChessPiece(
         "knight", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.C, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.C) == ChessPiece(
         "bishop", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.D, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.D) == ChessPiece(
         "queen", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.E, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.E) == ChessPiece(
         "king", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.F, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.F) == ChessPiece(
         "bishop", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.G, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.G) == ChessPiece(
         "knight", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.H, File.ONE) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.ONE, File.H) == ChessPiece(
         "rook", colour="white"
     )
-    assert chess_board.get_piece_at_position(Rank.A, File.TWO) == ChessPiece(
+    assert chess_board.get_piece_at_position(Rank.TWO, File.A) == ChessPiece(
         "pawn", colour="white"
     )
 
-    assert chess_board.get_piece_at_position(Rank.A, File.THREE) is None
+    assert chess_board.get_piece_at_position(Rank.THREE, File.A) is None
