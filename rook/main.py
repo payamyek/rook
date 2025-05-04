1ea75cd1efea8c56a5a034cdc0fea6d6ddf9f49d
Payam Yektamaram <payamyek@gmail.com>
2025-05-03 23:39:23 -0400

    feat: bitboard __str__

--- rook/main.py
+++ rook/main.py
@@ -1,22 +1,9 @@
-from rook.bitboard import Bitboard, File, Rank
+from bitboard import Bitboard
 
 
 def main():
     print("Hello from rook!")
-
-    bitboard = Bitboard()
-    # print(f"{bitboard.get_position_mask(Rank.A, File.EIGHT):>064b}")
-    # print(f"{bitboard.get_position_mask(Rank.B, File.EIGHT):>064b}")
-    # print(f"{bitboard.get_position_mask(Rank.A, File.SEVEN):>064b}")
-    # print(f"{bitboard.get_position_mask(Rank.H, File.ONE):>064b}")
-
-    print(bitboard.get_piece_at_position(Rank.A, File.EIGHT))
-    print(bitboard.get_piece_at_position(Rank.B, File.EIGHT))
-    print(bitboard.get_piece_at_position(Rank.B, File.SEVEN))
-    # print(f"{bitboard.black_pawn:064b}")
-    # print(f"{bitboard.white_pawn:064b}")
-    # print(len(str(f"{bitboard.black_pawn:064b}")))
-    # print(bitboard.white_pawn)
+    print(Bitboard())
 
 
 if __name__ == "__main__":
--- tests/test_bitboard.py
+++ tests/test_bitboard.py
@@ -1,5 +1,5 @@
 import pytest  # noqa: F401
-from rook.bitboard import Bitboard, File, Rank
+from rook.bitboard import Bitboard, File, Rank, ChessPiece
 
 
 def test_bitboard_get_position_mask():
@@ -24,3 +24,29 @@ def test_bitboard_get_position_mask():
         bitboard.get_position_mask(Rank.H, File.ONE)
         == 0b_00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000001
     )
+
+
+def test_bitboard_get_piece_at_position():
+    bitboard = Bitboard()
+
+    assert bitboard.get_piece_at_position(Rank.A, File.EIGHT) == ChessPiece.BLACK_ROOK
+    assert bitboard.get_piece_at_position(Rank.B, File.EIGHT) == ChessPiece.BLACK_KNIGHT
+    assert bitboard.get_piece_at_position(Rank.C, File.EIGHT) == ChessPiece.BLACK_BISHOP
+    assert bitboard.get_piece_at_position(Rank.D, File.EIGHT) == ChessPiece.BLACK_QUEEN
+    assert bitboard.get_piece_at_position(Rank.E, File.EIGHT) == ChessPiece.BLACK_KING
+    assert bitboard.get_piece_at_position(Rank.F, File.EIGHT) == ChessPiece.BLACK_BISHOP
+    assert bitboard.get_piece_at_position(Rank.G, File.EIGHT) == ChessPiece.BLACK_KNIGHT
+    assert bitboard.get_piece_at_position(Rank.H, File.EIGHT) == ChessPiece.BLACK_ROOK
+    assert bitboard.get_piece_at_position(Rank.A, File.SEVEN) == ChessPiece.BLACK_PAWN
+
+    assert bitboard.get_piece_at_position(Rank.A, File.ONE) == ChessPiece.WHITE_ROOK
+    assert bitboard.get_piece_at_position(Rank.B, File.ONE) == ChessPiece.WHITE_KNIGHT
+    assert bitboard.get_piece_at_position(Rank.C, File.ONE) == ChessPiece.WHITE_BISHOP
+    assert bitboard.get_piece_at_position(Rank.D, File.ONE) == ChessPiece.WHITE_QUEEN
+    assert bitboard.get_piece_at_position(Rank.E, File.ONE) == ChessPiece.WHITE_KING
+    assert bitboard.get_piece_at_position(Rank.F, File.ONE) == ChessPiece.WHITE_BISHOP
+    assert bitboard.get_piece_at_position(Rank.G, File.ONE) == ChessPiece.WHITE_KNIGHT
+    assert bitboard.get_piece_at_position(Rank.H, File.ONE) == ChessPiece.WHITE_ROOK
+    assert bitboard.get_piece_at_position(Rank.A, File.TWO) == ChessPiece.WHITE_PAWN
+
+    assert bitboard.get_piece_at_position(Rank.A, File.THREE) is None
